536fe00a36d83b80465845a2882fa599b97e20dc
ankitoscar <ankitoscar911@gmail.com>
2022-05-20 21:57:38 +0530

    Added toggleFlashMode function

--- entry/camerafragment/index.js
+++ entry/camerafragment/index.js
@@ -12,6 +12,7 @@ export default class CameraFragment {
         this.cameraId = this.cameraObj.cameraId;
         this.cameraPosition = this.cameraObj.cameraPosition;
         this.cameraType = this.cameraObj.cameraType;
+        this.flashMode = 0;
     }
 
     initialize(){
@@ -20,4 +21,21 @@ export default class CameraFragment {
                 return cameraManage.createCameraInput(this.cameraId)
             })
     }
+
+    // Function to toggle flash mode, if flash is set to last mode
+    // it will be set back to the first mode.
+    toggleFlashMode(){
+        if(this.cameraInput
+            .then((instance) => {return instance.hasFlash()
+                .then((status) => {return status})})){
+            this.cameraInput.then((instance) => {
+                return instance.setFlashMode((this.flashMode+1)%4)
+                    .then(() => {
+                        console.log("Flash Mode toggled.")
+                    })
+            })
+        }
+    }
+
+
 }
